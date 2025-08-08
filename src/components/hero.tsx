8edbf3737566ba69494360a76909a5957249c7b3
KuldeepJha5176 <ranacjha@gmail.com>
2025-08-08 23:40:22 +0530

    learned the bg cliping of text and also the gradient behind it

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
-      hero
+    <div className='my-40'>
+      <h1 className='text-5xl font-bold tracking-tight max-w-xl leading-tight text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-500'>
+      Unleash the power of intuitive finance
+
+      </h1>
     </div>
   )
 }
